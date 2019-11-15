9249e6c2bdc6e95a3c4fc3e970d099249a8271d4
iamdanta <984611326@qq.com>
2019-11-15 11:50:48 +0800

    feat 基本内容

--- README.md
+++ README.md
@@ -1,29 +1,7 @@
 # vue-framework
 
-## Project setup
+## 项目简介
 ```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
+none
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+import store from './store';
+import router from './router';
+
+// 插件
+import './plugins/element.js';
+
+// 样式
+import './styles/index.scss';
+
+// 开发环境配置（如果需要就去掉注释）
+// if (process.env.NODE_ENV === 'development') {
+//   // mock 服务
+//   require('./mock');
+// }
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  store,
+  router,
+  render: (h) => h(App)
+});
